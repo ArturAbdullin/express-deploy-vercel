f52546427cadd5bf6d49af5b2b9c87fa8ebae10c
Artur Abdullin <artur.abdullin@gmail.com>
2022-11-24 19:11:14 +0300

    feat: add cors origin

--- server.js
+++ server.js
@@ -3,7 +3,11 @@ const cors = require("cors");
 const memberRouter = require("./src/member/routes");
 
 const app = express();
-app.use(cors());
+app.use(
+  cors({
+    origin: "https://arturabdullin.github.io/fetch-vercel/",
+  })
+);
 app.use("/members", memberRouter);
 
 module.exports = app;

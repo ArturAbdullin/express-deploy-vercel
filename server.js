9eaf8d02db8fbd67b6c6e67ff4f7aa84a1944d97
Artur Abdullin <artur.abdullin@gmail.com>
2022-11-24 18:16:12 +0300

    feat: add cors

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require("express");
+const cors = require("cors");
 const memberRouter = require("./src/member/routes");
 
 const app = express();
+app.use(cors());
 app.use("/members", memberRouter);
 
 module.exports = app;
